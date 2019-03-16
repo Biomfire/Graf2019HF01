ae31178a54ede9f05d8f9f24191bd51e16d2b01b
Kiss Benedek <kissbence111@gmail.com>
2019-03-16 20:58:04 +0100

    Add project files.

---
+++ Graf2019HF01.cpp
@@ -0,0 +1,12 @@
+﻿// Graf2019HF01.cpp : Defines the entry point for the application.
+//
+
+#include "Graf2019HF01.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
